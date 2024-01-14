525cbd690c1f64ad999fb104e40ede796d4ee448
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2024-01-14 17:25:53 +0900

    buyItemConfirm機能の確認の確認までOK

--- src/com/diworksdev/template/action/UserCreateAction.java
+++ src/com/diworksdev/template/action/UserCreateAction.java
@@ -1,8 +1,14 @@
 package com.diworksdev.template.action;
+//このアクションクラスが所属するパッケージを定義しています。
 import com.opensymphony.xwork2.ActionSupport;
-
+//Struts 2 フレームワークの ActionSupport クラスをインポートしています。
+//ActionSupport は Struts 2 アクションの基本クラス(デフォルトであるクラス)で、共通の機能やメソッドを提供します。
 public class UserCreateAction extends ActionSupport {
+//	クラスを定義して ActionSupportクラスを継承しています。
 	public String execute() {
+//		execute メソッドは、Struts 2 アクションクラス内で定義されるメソッドです。
+//		このメソッドはアクションを実行するときに呼び出されます。アクションがリクエストを処理し、結果を返すためのロジックが通常このメソッド内に書かれます
 		return SUCCESS;
+//		アクションが正常に完了した場合、SUCCESS という文字列を返しています。
 		}
 }
